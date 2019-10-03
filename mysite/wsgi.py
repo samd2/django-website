5bc37bc255d545d597529f8fa50bc5e20b85d832
sdarwin <samuel.d.darwin@gmail.com>
2019-10-03 17:09:22 +0000

    dotenv path

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 import dotenv
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv(dotenv_path="/home/django/repo/.env")
 
 from django.core.wsgi import get_wsgi_application
 
